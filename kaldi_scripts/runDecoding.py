693741b1fe3ad2829016a6fd664cc78a52168b6d
Brian Ogbebor <bbrian125@gmail.com>
2020-03-18 18:50:05 -0400

    Working with some audio -Brian

--- kaldi_scripts/runDecoding.py
+++ kaldi_scripts/runDecoding.py
@@ -3,14 +3,41 @@
     - This script aims to pipe audio to the tcp server!
 '''
 
-import os
+
 import subprocess
+import sys
 import pyaudio
-from pathlib import Path
+import numpy as np
+import pylab
+import time
+import matplotlib.pyplot as plt
 
-p1 = subprocess.Popen(["cat", "setUp.py"],
-                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
-stdout, stderr = p1.communicate()
+RATE = 44100
+CHUNK = int(RATE/20)  # RATE / number of updates per second
 
-print(stdout.decode())
+
+def soundplot(stream):
+    t1 = time.time()
+    data = np.fromstring(stream.read(CHUNK), dtype=np.int16)
+    print(data)
+
+
+if __name__ == "__main__":
+    p = pyaudio.PyAudio()
+    stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True,
+                    frames_per_buffer=CHUNK)
+    for i in range(sys.maxsize**10):
+        soundplot(stream)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+
+
+#os.system("sox ~/Desktop/brian_flip_on_the_lights.wav -t raw -c 1 -b 16 -r 8k -e signed-integer - | nc 35.245.124.99 5050")
+
+# p1 = subprocess.Popen("sox brian_flip_on_the_lights.wav -t raw -c 1 -b 16 -r 8k -e signed-integer -",
+#                      stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+# process = subprocess.Popen(
+#    ["nc", "35.245.124.99", "5050"], stdin=p1.stdout, stdout=subprocess.PIPE)
